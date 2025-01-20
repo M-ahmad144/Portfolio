c80059d2821987919d44094279f5d9d7837a3f1d
M-ahmad144 <muhammadahmadmughal0987@gmail.com>
2025-01-20 19:50:47 +0500

    About.jsx

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
+import About from "./components/about/About.jsx";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home.jsx";
-
+import { BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
     <>
-      <Header />
-      <main className="main">
-        <Home />
-      </main>
+      <Router>
+        {" "}
+        <Header />
+        <main className="main">
+          <Home />
+          <About />
+        </main>
+      </Router>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Ensure that the root element exists in your HTML
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Rendering the React App wrapped with StrictMode for development
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Measuring performance (optional)
 reportWebVitals();
